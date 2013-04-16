8f7cd098d11c2fe046692f3c9664c126157c288f
Alex Blunk <alex@beelinerco.com>
2013-04-15 23:11:01 -0700

    Initial Page Production

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Beeline::Application.routes.draw do
+  get 'about' => 'pages#about'
+
+  root :to => 'pages#home'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
