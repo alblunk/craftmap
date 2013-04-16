025050f6666c77c97d5ece0fa3b1fb067aa04007
Alex Blunk <alex@beelinerco.com>
2013-04-15 22:37:00 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beeline::Application.config.secret_token = 'eccb924deb1929fc968f2b54e4842227b3f689398281a3feb411ec44ef29fc7524ccfe583954173a9e9261763a880e979339ac99b64242847b586b75adf0c648'
