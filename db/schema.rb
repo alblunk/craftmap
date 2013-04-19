16723d322f421b68e2546ab709f4d29faf3fa8fc
Alex Blunk <alex@beelinerco.com>
2013-04-18 23:56:17 -0700

    Added Admin Functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130418101447) do
+ActiveRecord::Schema.define(:version => 20130419061123) do
 
   create_table "products", :force => true do |t|
     t.string   "description"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20130418101447) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
