faf4959b606b0b287818e4131d32e027e622a421
Alex Blunk <alex@beelinerco.com>
2013-04-24 22:45:50 -0700

    Homepage Styling

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,7 @@ class Product < ActiveRecord::Base
                             size: { less_than: 5.megabytes }
   
   belongs_to :user
-  has_attached_file :image, styles: { medium: "320x240>"}
+  has_attached_file :image, styles: { medium: "360x270>"}
 
   def image_remote_url=(url_value)
   	self.image = URI.parse(url_value) unless url_value.blank?
