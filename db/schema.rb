7cd7b76fa57e8ef96d327404873ff9fb94e9fa46
Mike Chlipala <mike@chlipala.com>
2013-08-09 11:36:18 -0700

    add brand model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130805013653) do
+ActiveRecord::Schema.define(version: 20130809182440) do
+
+  create_table "brands", force: true do |t|
+    t.string   "name"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "product_images", force: true do |t|
     t.integer  "product_id"
