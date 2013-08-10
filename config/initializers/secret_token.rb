67d68528bb36d35196098fa1849a4ef67f3ad61b
Mike Chlipala <mike@chlipala.com>
2013-08-09 17:01:10 -0700

    add secret_key_base to suppress error messages

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Beeline::Application.config.secret_token = 'eccb924deb1929fc968f2b54e4842227b3f689398281a3feb411ec44ef29fc7524ccfe583954173a9e9261763a880e979339ac99b64242847b586b75adf0c648'
+Beeline::Application.config.secret_key_base = '[dka03dk;sduj'
