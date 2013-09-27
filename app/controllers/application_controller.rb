1b409abaa339259cd36b2c18eafd509decd58d2e
Mike Chlipala <mike@chlipala.com>
2013-09-27 17:29:27 -0500

    stealth mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :stealth_mode
+
   def admin?
     current_user? && current_user.admin?
   end
@@ -16,6 +18,13 @@ class ApplicationController < ActionController::Base
 
       unless admin?
         redirect_to root_path
+      end
+    end
+
+    def stealth_mode
+      if Rails.env.production?
+        redirect_to root_path unless request.path =~ /^\/$/
+        return
       end
     end
 end
