1810df75569f6300f217a432c7bfb902b9617b0c
Matt Freeman <rmf34@cornell.edu>
2013-09-29 13:20:24 -0700

    Add paperclip initializer file. Unsure if need other config.

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,4 @@
+# unsure if these variables need to be changed, or if they are just for custom configurations
+
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
