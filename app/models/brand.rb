4833098093f29f85f4babe9e187b8a81da09bc83
Matt Freeman <rmf34@cornell.edu>
2013-10-01 00:09:56 -0700

    Set current user to brand owner upon brand creation.

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,6 +2,7 @@ class Brand < ActiveRecord::Base
   validates :name, presence: true
   validates :profile, presence: true
 
+  belongs_to :owner, class_name: "User", foreign_key: :owner_id
   has_many :products
   # other side of polymorphic images
   has_many :images, as: :imagable#, dependent: :destroy
