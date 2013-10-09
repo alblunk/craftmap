5adf8b0610a86467b156b238c80db24043d72ca7
Alex Blunk <alex@farallon.co>
2013-10-09 10:19:54 -0700

    landing page copy changes

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,32 +1,12 @@
-// if ($(window).width() < 960) {
-//     alert('Less than 960');
-
-// }
-// else {
-//     alert('More than 960');
-//     eventFired = 1;
-// }
-
-// $(window).on('resize', function() {
-//     if (!eventFired) {
-//         if ($(window).width() < 960) {
-//             alert('Less than 960 resize');
-//         } else {
-//             alert('More than 960 resize');
-//         }
-//     }
-// });
-
-
 $(document).ready(function(){
     if ($(window).width() > 991) {
          $('.product-grid').mouseenter(function() {
             $('.p-hover').fadeTo('fast', 1);
-            $('#motto').fadeTo('fast', 0);
+            $('#motto').hide()
         });  
         $('.product-grid').mouseleave(function(){
-            $('.p-hover').fadeTo('fast', 0);
-            $('#motto').fadeTo('fast', 1);
+            $('.p-hover').hide();
+            $('#motto').show();
         });
     }
     else {
@@ -35,3 +15,10 @@ $(document).ready(function(){
     }
 });
 
+// $('.product-grid').mouseenter(function() {
+//             $('.p-hover').fadeTo('fast', 1);
+//             $('#motto').fadeTo('fast', 0);
+//         });  
+//         $('.product-grid').mouseleave(function(){
+//             $('.p-hover').fadeTo('fast', 0);
+//             $('#motto').fadeTo('fast', 1);
