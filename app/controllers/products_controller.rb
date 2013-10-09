cdeed7ee0b47462957b6e58b2856a2d950e7a951
Alex Blunk <alex@farallon.co>
2013-10-08 22:52:57 -0700

    header & product table tweak

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @products = Product.order("created_at desc").page(params[:page]).per(4)
+    @products = Product.order("created_at desc").page(params[:page]).per(6)
     @brand = @product.brand
   end
 
