23e1cb8486c0de7abfac126cc70e37f69d95f847
Mike Chlipala <mike@chlipala.com>
2013-10-21 19:28:25 -0700

    remove masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.masonry.min.js
 //= require twitter/bootstrap
 //= require bootstrap-wysihtml5/b3
 //= require bootstrap-wysihtml5/locales
