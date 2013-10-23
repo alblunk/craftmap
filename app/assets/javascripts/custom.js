7afdc5282be4d032614484ce9da614f2f8f176ad
Alex Blunk <alex@farallon.co>
2013-10-22 17:52:57 -0700

    turning off jquery hover on home for now

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -11,20 +11,20 @@ $(document).ready(function(){
 
 
 
-    if ($(window).width() > 991) {
-         $('.product-grid').mouseenter(function() {
-            $('.p-hover').fadeTo('fast', 1);
-            $('#motto').hide()
-        });  
-        $('.product-grid').mouseleave(function(){
-            $('.p-hover').hide();
-            $('#motto').show();
-        });
-    }
-    else {
-        $('.p-hover').hide();
-        $('#motto').show();
-    }
+    // if ($(window).width() > 991) {
+    //      $('.product-grid').mouseenter(function() {
+    //         $('.p-hover').fadeTo('fast', 1);
+    //         $('#motto').hide()
+    //     });  
+    //     $('.product-grid').mouseleave(function(){
+    //         $('.p-hover').hide();
+    //         $('#motto').show();
+    //     });
+    // }
+    // else {
+    //     $('.p-hover').hide();
+    //     $('#motto').show();
+    // }
 });
 
 // $('.product-grid').mouseenter(function() {
