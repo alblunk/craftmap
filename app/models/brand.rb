f8562ab2bfbc18d9c9b2a47b609c7f68db73475a
Mike Chlipala <mike@chlipala.com>
2013-10-27 14:02:29 -0700

    delete associated products when brands are deleted

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,7 +3,7 @@ class Brand < ActiveRecord::Base
   validates :profile, presence: true
 
   belongs_to :owner, class_name: "User", foreign_key: :owner_id
-  has_many :products
+  has_many :products, :dependent => :destroy
 
   has_one :profile_image, as: :imageable, :class_name => 'SecondaryImage'
   accepts_nested_attributes_for :profile_image, allow_destroy: true, :reject_if => lambda {|a| a['image'].blank?}
