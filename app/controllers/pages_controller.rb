39bba5a7ff7074f09aa8b8dda2e07a6c2cda678e
Alex Blunk <alex@farallon.co>
2013-10-28 17:09:21 -0700

    pass change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -27,7 +27,7 @@ class PagesController < ApplicationController
 
     def authenticate
       authenticate_or_request_with_http_basic do |username, password|
-        username == "brands" && password == "farallon"
+        username == "brands" && password == "budzo"
       end
     end
 end
