f9be600590e33c3cfad9c8689aab3f70550e802f
Alex Blunk <alex@farallon.co>
2013-11-02 11:28:25 -0700

    Revert "hover over product will show product details"
    This reverts commit ba990c3c7328369cff41d9db9819d0f05a45c026.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,35 +1,33 @@
 $(document).ready(function(){
 
-  $('#brand-sort').click(function() {
-    $('#home-brands').show();
-    $('#home-products').hide()
-  });
-  $('#product-sort').click(function(){
-    $('#home-brands').hide();
-    $('#home-products').show();
-  });
-
-  $('#thumbnail-hide').click(function() {
-    $('#p-thumbnails-box').toggle("fast");
-    $('#thumbnail-hide').text($('#thumbnail-hide').text() == 'SHOW' ? 'HIDE' : 'SHOW');
-  });
-
-  if ($(window).width() > 991) {
-    $('#home-products .thumbnail').mouseenter(function() {
-      // Hide what's already shown.
-      $(".p-hover").not(".p-hover .hidden").addClass("hidden")
-
-      // Find what we should show.
-      var productID = $(this).data("product-id");
-      var e = $('.p-hover[data-product-id="' + productID +  '"]').removeClass('hidden')
-      $('#motto').hide()
+    $('#brand-sort').click(function() {
+        $('#home-brands').show();
+        $('#home-products').hide()
+    });  
+    $('#product-sort').click(function(){
+        $('#home-brands').hide();
+        $('#home-products').show();
     });
-    $('#home-products .thumbnail').mouseenter(function() {
-  }
-  else {
-    $('.p-hover').hide();
-    $('#motto').show();
-  }
+
+    $('#thumbnail-hide').click(function() {
+        $('#p-thumbnails-box').toggle("fast");
+        $('#thumbnail-hide').text($('#thumbnail-hide').text() == 'SHOW' ? 'HIDE' : 'SHOW');
+    });
+
+    // if ($(window).width() > 991) {
+    //      $('#home-products').mouseenter(function() {
+    //         $('.p-hover').fadeTo('fast', 1);
+    //         $('#motto').hide()
+    //     });  
+    //     $('#home-products').mouseleave(function(){
+    //         $('.p-hover').hide();
+    //         $('#motto').show();
+    //     });
+    // }
+    // else {
+    //     $('.p-hover').hide();
+    //     $('#motto').show();
+    // }
 });
 
 // $('.product-grid').mouseenter(function() {
