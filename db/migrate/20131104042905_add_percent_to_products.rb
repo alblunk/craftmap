220829b2e2e1304b2c1e5b4f51c068740c58de02
Alex Blunk <alex@farallon.co>
2013-11-03 21:04:47 -0800

    delete migrations

--- db/migrate/20131104042905_add_percent_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddPercentToProducts < ActiveRecord::Migration
-  def change
-    add_column :products, :percent, :integar
-  end
-end
--- db/migrate/20131104044318_rename_percent_to_products.rb
+++
@@ -1,5 +0,0 @@
-class RenamePercentToProducts < ActiveRecord::Migration
-  def change
-    rename_column :products, :percent, :integer
-  end
-end
