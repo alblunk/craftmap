0a1e2edf59512fc2391ce8eb2cc640e60f85ce17
Alex Blunk <alex@farallon.co>
2013-11-12 20:11:48 -0800

    shiz

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ActionMailer::Base
-  default from: 'info@farllon.co'
+  default from: 'hi@farallon.co'
 
   def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Welcome to Farallon')
+    mail(to: @user.email, subject: 'Welcome to Farallon.')
   end
 end
