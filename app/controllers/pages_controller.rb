3408cd026e5902855e04483d35d6b00e28b2c8e8
Alex Blunk <alex@farallon.co>
2014-02-19 11:40:29 -0800

    shiz

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -31,7 +31,7 @@ class PagesController < ApplicationController
   end
 
   def all_products
-    @products = Product.active_products.order("created_at desc")
+    @products = Product.active_products.order("created_at desc").page(params[:page]).per(60)
   end
 
 
